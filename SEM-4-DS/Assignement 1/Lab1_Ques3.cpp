6f13d46b548b21637fe4cb1b5ba8483413fd1b1d
Rishabh Gupta <rgupta8_be24@thapar.edu>
2026-01-28 11:01:28 +0530

    Update Lab1_Ques3.cpp

--- SEM-4-DS/Assignement 1/Lab1_Ques3.cpp
+++ SEM-4-DS/Assignement 1/Lab1_Ques3.cpp
@@ -1,1 +1,42 @@
-ok
+#include<bits/stdc++.h>
+using namespace std;
+
+pair<int, int> maxSubarraySum(vector<int>& arr, int n){
+    int maxSum = INT_MIN;
+    int sum = 0;
+    int start, end;
+    for(int i=0; i<n; i++){
+        sum += arr[i];
+        if(sum < 0){
+            sum = 0;
+            start = i+1;
+        }
+        if(sum > maxSum){
+            maxSum = sum;
+            end = i;
+        }
+    }
+    return {start, end};
+}
+
+int main(){
+    int n;
+    cout << "Enter number of elements : ";
+    cin >> n;
+    vector<int> arr(n);
+    cout << "Enter array elements :" << endl;
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+    cout << "Max subarray :" << endl;
+    int start = maxSubarraySum(arr, n).first;
+    int end = maxSubarraySum(arr, n).second;
+    int sum = 0;
+    for(int i=start; i<=end; i++){
+        sum += arr[i];
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    cout << "Max subarray sum :" << sum << endl;
+    return 0;
+}
